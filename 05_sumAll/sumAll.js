3c63d4d5bd6a433094442e4de6a2be252ed21de2
SLajuwomi <stephenlajuwomi@gmail.com>
2024-07-04 10:21:33 -0500

    Completed Exercis #5 using a nested for loop (Quite inefficient, but it works)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start != "number" || typeof end != "number") {
+        return "ERROR"
+    }
+    if (start < 0 || end <0) {
+        return ("ERROR")
+    }
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let i=start;
+    let numRange = [start];
+    let currentSum;
+    let finalSum;
+    while (i<end) {
+        numRange.push(i+=1)
+    }
 
+    for (let x = 0; x<numRange.length; x++) {
+        for (let j=1; j<numRange.length; j++) {
+            if (currentSum == null) {
+                currentSum = numRange[x] + numRange[j]
+            } else {
+                currentSum = currentSum + numRange[j]
+            }
+        }
+        break;
+    }
+    finalSum = currentSum;
+    console.log(finalSum)
+    return finalSum;
 };
 
 // Do not edit below this line
